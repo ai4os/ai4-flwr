6753805d34ffc6ca7ba3f428cb3b9f8fb12a2eb3
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2024-01-16 13:31:27 +0100

    feat: include initial Vault implementation

---
+++ src/ai4flwr/auth/vault.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2019 Spanish National Research Council (CSIC)
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""Authentication for Flower server, using Vault secrets as Bearer tokens."""
+
+import functools
+from logging import ERROR, INFO
+import typing
+
+from flwr.common.logger import log
+import hvac
+import jwt
+
+from ai4flwr.auth import bearer
+
+
+class VaultBearerTokenInterceptor(bearer.BearerTokenInterceptor):
+    """Vault Bearer token interceptor.
+
+    This class will retrieve the list of tokens from Vault, and will
+    authenticate the call if the token is on the list.
+    """
+
+    def __init__(
+        self,
+        vault_addr: str,
+        oidc_access_token: str,
+        deployment_id: str,
+        vault_role: typing.Optional[str] = "",
+        vault_auth_path: typing.Optional[str] = "jwt",
+        vault_mountpoint: typing.Optional[str] = "/secrets/",
+    ):
+        """Initialize VaultBearerTokenInterceptor.
+
+        :param vault_addr: Vault address
+        :param oidc_access_token: OIDC access token to authenticate with Vault
+        :param de
+        """
+
+        # FIXME(aloga): the token will expire after some time, we should
+        # reauthenticate, or use Vault + Nomad integration to avoid using the oidc
+        # token at all
+        self._client = hvac.Client(url=vault_addr)
+        self._client.auth.jwt.jwt_login(
+            role=vault_role,
+            jwt=oidc_access_token,
+            path=vault_auth_path
+        )
+        self._client.secrets.kv.default_kv_version = 1
+
+        # FIXME(aloga): can we use v2 and configure the mountpoint?
+        self._vault_mountpoint = vault_mountpoint
+
+        user_id = self._get_user_id(oidc_access_token)
+
+        home_path = f"users/{user_id}/"
+        self._vault_path = f"{home_path}/deployments/{deployment_id}/federated/"
+
+        log(INFO, "Configured Vault Bearer token authentication with: '%s'", vault_addr)
+        log(INFO, "Reading tokens stored in: '%s'", self._vault_path)
+        try:
+            log(INFO, "Configured Vault Bearer tokens: '%s'", self.tokens)
+        except Exception as e:
+            log(ERROR, "Error reading tokens from Vault: '%s'", e)
+            raise
+
+    def _get_user_id(self, token: str) -> str:
+        try:
+            payload = jwt.decode(token, options={"verify_signature": False})
+            return payload.get("sub")
+        except (jwt.InvalidTokenError, jwt.ExpiredSignatureError) as e:
+            log(ERROR, "Invalid OIDC token: '%s'", token)
+            raise e
+
+    # FIXME(aloga): this should be cached, but we need to invalidate the cache
+    # when a new token is added or a new token is removed. Morever, since authentication
+    # based on OIDC tokens againt Vault will stop working after some time (i.e. when
+    # the OIDC token expires), we can use this function for testing, but we should
+    # not use it in production.
+    @functools.cached_property
+    def tokens(self) -> typing.List[str]:
+        """Get list of tokens from Vault.
+
+        :returns: list of tokens
+        """
+        print("Getting tokens from Vault -> ", self._vault_path)
+        response = self._client.secrets.kv.list_secrets(
+            mount_point=self._vault_mountpoint,
+            path=self._vault_path,
+        )
+        secrets = response.get("data", {}).get("keys", [])
+
+        tokens = []
+        for s in secrets:
+            secret = self._client.secrets.kv.read_secret(
+                mount_point=self._vault_mountpoint,
+                path=f"{self._vault_path}/{s}",
+            )
+            token = secret.get("data", {}).get("token")
+            if token:
+                tokens.append(token)
+        return tokens
