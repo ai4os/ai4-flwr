e3a2fc878d2625bc2f1ebd13688f56245982f5d4
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2024-02-07 12:17:27 +0100

    feat: Improve Vault code and classes
     - Add a base class for Vault
     - Add a class to use Vault tokens for authentication
    
    fix

--- src/ai4flwr/auth/vault.py
+++ src/ai4flwr/auth/vault.py
@@ -16,6 +16,7 @@
 
 """Authentication for Flower server, using Vault secrets as Bearer tokens."""
 
+import abc
 import functools
 from logging import ERROR, INFO
 import typing
@@ -27,7 +28,112 @@ import jwt
 from ai4flwr.auth import bearer
 
 
-class OIDCVaultBearerTokenInterceptor(bearer.BearerTokenInterceptor):
+class BaseVaultBearerTokenInterceptor(bearer.BearerTokenInterceptor, abc.ABC):
+    """Vault Bearer token interceptor, abstract class.
+
+    This class will retrieve the list of tokens from Vault, and will
+    authenticate the call if the token is on the list.
+
+    This class is abstract, and should be subclassed to implement the
+    authentication method. Subclasses should set the _client attribute
+    to a valid Vault client before calling the parent __init__ method.
+    """
+
+    def __init__(
+        self,
+        vault_mountpoint: typing.Optional[str] = "/secrets/",
+        secret_path: typing.Optional[str] = "federated",
+    ):
+        if self._client is None:
+            raise ValueError("Vault client is not initialized")
+
+        self._client.secrets.kv.default_kv_version = 1
+
+        self._secret_path = secret_path
+        self._vault_mountpoint = vault_mountpoint
+
+        vault_addr = self._client.url
+
+        log(INFO, "Configured Vault Bearer token authentication with: '%s'", vault_addr)
+        log(INFO, "Reading tokens stored in: '%s'", self._secret_path)
+        try:
+            log(INFO, "Configured Vault Bearer tokens: '%s'", self.tokens)
+        except Exception as e:
+            log(ERROR, "Error reading tokens from Vault: '%s'", e)
+            raise
+
+    # FIXME(aloga): this should be cached, but we need to invalidate the cache
+    # when a new token is added or a new token is removed.
+    @functools.cached_property
+    def tokens(self) -> typing.List[str]:
+        """Get list of tokens from Vault.
+
+        :returns: list of tokens
+        """
+        print("Getting tokens from Vault -> ", self._secret_path)
+        response = self._client.secrets.kv.list_secrets(
+            mount_point=self._vault_mountpoint,
+            path=self._secret_path,
+        )
+        secrets = response.get("data", {}).get("keys", [])
+
+        tokens = []
+        for s in secrets:
+            secret = self._client.secrets.kv.read_secret(
+                mount_point=self._vault_mountpoint,
+                path=f"{self._secret_path}/{s}",
+            )
+            token = secret.get("data", {}).get("token")
+            if token:
+                tokens.append(token)
+        return tokens
+
+    async def renew(self, increment: typing.Optional[str] = "768h"):
+        """Renew the Vault token, increase its validity to the duration given in the
+        increment parameter.
+
+        :param increment: Renew duration, e.g. ’15s’, ‘20m’, ‘25h’.
+        """
+        self._client.auth.token.renew_self(increment=increment)
+
+        # Print debug info
+        response = self._client.auth.token.lookup_self()
+        ttl = response.get("data").get("ttl")
+        log(INFO, "Vault token is valid for %d seconds", ttl)
+
+
+class VaultBearerTokenInterceptor(BaseVaultBearerTokenInterceptor):
+    """Vault Bearer token interceptor with Vault token authentication.
+
+    This class will retrieve the list of tokens from Vault, and will
+    authenticate the call if the token is on the list.
+    """
+
+    def __init__(
+        self,
+        vault_addr: str,
+        vault_token: str,
+        vault_mountpoint: typing.Optional[str] = "/secrets/",
+        secret_path: typing.Optional[str] = "federated",
+    ):
+        """Initialize VaultBearerTokenInterceptor.
+
+        :param vault_addr: Vault address
+        :param vault_token: Vault token to authenticate with
+        :param vault_mountpoint: Vault mountpoint
+        :param secret_path: Vault path to read tokens from. We will look for secrets
+                            stored under that path, if they contain a "token" key, that
+                            will be used as bearer tokens.
+        """
+        self._client = hvac.Client(url=vault_addr, token=vault_token)
+
+        super(self.__class__, self).__init__(
+            vault_mountpoint=vault_mountpoint,
+            secret_path=secret_path,
+        )
+
+
+class OIDCVaultBearerTokenInterceptor(BaseVaultBearerTokenInterceptor):
     """Vault Bearer token interceptor with OIDC authentication.
 
     This class will retrieve the list of tokens from Vault, and will
@@ -55,73 +161,15 @@ class OIDCVaultBearerTokenInterceptor(bearer.BearerTokenInterceptor):
                             will be used as bearer tokens.
         """
 
-        # FIXME(aloga): the token will expire after some time, we should
-        # reauthenticate, or use Vault + Nomad integration to avoid using the oidc
-        # token at all
         self._client = hvac.Client(url=vault_addr)
         self._client.auth.jwt.jwt_login(
-            role=vault_role,
-            jwt=oidc_access_token,
-            path=vault_auth_path
+            role=vault_role, jwt=oidc_access_token, path=vault_auth_path
         )
-        self._client.secrets.kv.default_kv_version = 1
 
-        # FIXME(aloga): can we use v2 and configure the mountpoint?
-        self._vault_mountpoint = vault_mountpoint
-
-        self._secret_path = secret_path
-
-        log(INFO, "Configured Vault Bearer token authentication with: '%s'", vault_addr)
-        log(INFO, "Reading tokens stored in: '%s'", self._secret_path)
-        try:
-            log(INFO, "Configured Vault Bearer tokens: '%s'", self.tokens)
-        except Exception as e:
-            log(ERROR, "Error reading tokens from Vault: '%s'", e)
-            raise
-
-    # FIXME(aloga): this should be cached, but we need to invalidate the cache
-    # when a new token is added or a new token is removed. Morever, since authentication
-    # based on OIDC tokens againt Vault will stop working after some time (i.e. when
-    # the OIDC token expires), we can use this function for testing, but we should
-    # not use it in production.
-    @functools.cached_property
-    def tokens(self) -> typing.List[str]:
-        """Get list of tokens from Vault.
-
-        :returns: list of tokens
-        """
-        print("Getting tokens from Vault -> ", self._secret_path)
-        response = self._client.secrets.kv.list_secrets(
-            mount_point=self._vault_mountpoint,
-            path=self._secret_path,
+        super(self.__class__, self).__init__(
+            vault_mountpoint=vault_mountpoint,
+            secret_path=secret_path,
         )
-        secrets = response.get("data", {}).get("keys", [])
-
-        tokens = []
-        for s in secrets:
-            secret = self._client.secrets.kv.read_secret(
-                mount_point=self._vault_mountpoint,
-                path=f"{self._secret_path}/{s}",
-            )
-            token = secret.get("data", {}).get("token")
-            if token:
-                tokens.append(token)
-        return tokens
-
-    def renew(
-        self,
-        increment: typing.Optional[str] = "768h",
-    ):
-        """ Renew the Vault token, increase its validity to the duration given in the increment parameter.
-
-        :param increment: Renew duration, e.g. ’15s’, ‘20m’, ‘25h’.
-        """
-        self._client.auth.token.renew_self(increment=increment)
-
-        # Print debug info
-        response = self._client.auth.token.lookup_self()
-        ttl = response.get("data").get("ttl")
-        log(INFO, "Vault token is valid for %d seconds", ttl)
 
 
 def get_user_id(token: str) -> str:
