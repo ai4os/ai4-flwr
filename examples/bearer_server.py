752ee0b4d6c91787fa3389f189ea4efda1c18f7c
Judith Sáinz-Pardo Díaz <sainzpardo@ifca.unican.es>
2024-02-08 09:55:03 +0100

    Update example

--- examples/bearer_server.py
+++ examples/bearer_server.py
@@ -3,7 +3,11 @@ import sys
 
 import ai4flwr.auth.bearer as bearer
 
+from logging import ERROR, INFO
+
 import flwr as fl
+
+FEDERATED_METRIC = "accuracy"
 
 if len(sys.argv) != 2:
     print("Error, No token has been provided!!!")
@@ -12,16 +16,30 @@ if len(sys.argv) != 2:
 
 interceptor = bearer.BearerTokenInterceptor(sys.argv[1])
 
-
-def wavg_accuracy(metrics):
-    n = sum([i for i, _ in metrics])
-    wavg_metric = sum([i * metric["accuracy"] / n for i, metric in metrics])
-    return {"accuracy": wavg_metric}
+def wavg_metric(metrics):
+    global FEDERATED_METRIC
+    list_metrics = []
+    try:
+        list_metrics = ast.literal_eval(FEDERATED_METRIC)
+    except ValueError:
+        log(INFO, "Only one metric has been entered.")
+    if len(list_metrics) == 0:
+        n = sum([i for i, _ in metrics])
+        wavg_metric = sum([i * metric[FEDERATED_METRIC] / n for i, metric in metrics])
+        return {FEDERATED_METRIC: wavg_metric}
+    else:
+        n = sum([i for i, _ in metrics])
+        dict_metrics = {}
+        for fed_metric in list_metrics:
+            wavg_metric = sum([i * metric[fed_metric] / n for i, metric in metrics])
+            dict_metrics[fed_metric] = wavg_metric
+        return dict_metrics
 
 
 strategy = fl.server.strategy.FedAvg(
     min_available_clients=2,
-    evaluate_metrics_aggregation_fn=wavg_accuracy,
+    min_fit_clients=2,
+    evaluate_metrics_aggregation_fn=wavg_metric,
 )
 
 server = fl.server.start_server(
